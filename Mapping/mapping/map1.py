4764d9b68348d9087137bf4589ea40b6dbb2f804
Christopher Lock <chrislock001@gmail.com>
2018-02-21 10:05:34 -0700

    Update map1.py

--- Mapping/mapping/map1.py
+++ Mapping/mapping/map1.py
@@ -18,11 +18,21 @@ def color_producer(elevation):
 
 map = folium.Map(location = [35.082225, -106.611677], zoom_start = 5, tiles = "Mapbox Bright")
 
-fg = folium.FeatureGroup(name = "My Map")
+fgv = folium.FeatureGroup(name = "Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-	fg.add_child(folium.Marker(location = [lt, ln], popup = str(el)+" m", icon = folium.Icon(color = color_producer(el))))
+	fgv.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, popup = str(el)+" m",
+	fill_color = color_producer(el), color = 'grey', fill = True, fill_opacity = 0.7))
+
+fgp = folium.FeatureGroup(name = "Population")	
 	
-map.add_child(fg)
+fgp.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(),
+style_function = lambda x: {'fillColor': 'green' if x ['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x ['properties']['POP2005'] < 20000000 else 'red'}))
+	
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
